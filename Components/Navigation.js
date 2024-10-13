149e81e04e8e4593eb56eaf61181ef41df6e02ab
Rheilly Aguilar <rheilly17@gmail.com>
2024-10-13 18:01:38 +0800

    add productnav and  page

--- Components/Navigation.js
+++ Components/Navigation.js
@@ -17,7 +17,7 @@ export class Navigation extends HTMLElement {
         const ul = document.createElement('ul');
         ul.classList.add('ul');
 
-        const link = ['Home', 'About', 'Service'];
+        const link = ['Home', 'Product', 'About', 'Service'];
         link.forEach(item => {
             const li = document.createElement('li');
             const a = document.createElement('a');
