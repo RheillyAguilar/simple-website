84d2b6a0f7ef1edb986a03001bafc16ef42d1669
Rheilly Aguilar <rheilly17@gmail.com>
2024-10-14 11:05:21 +0800

    fixing the directory

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 
 import { Navigation } from "./Components/Navigation.js";
-import { Home } from "./Components/Home.js";
-import { Card } from "./Components/Card.js";
-import { Quote } from "./Components/Quote.js";
-import { OtherCard } from "./Components/OtherCard.js";
+import { Home } from "./Components/Home/Home.js";
+import { Card } from "./Components/Home/Card.js";
+import { Quote } from "./Components/Home/Quote.js";
+import { OtherCard } from "./Components/Home/OtherCard.js";
 import { Footer } from "./Components/Footer.js";
-import { Info } from "./Components/Info.js";
-import { Service } from "./Components/Service.js";
-import { Values } from "./Components/Values.js";
-import { Product } from "./Components/Product.js";
+import { Info } from "./Components/About/Info.js";
+import { Service } from "./Components/Service/Service.js";
+import { Values } from "./Components/About/Values.js";
+import { Product } from "./Components/Product/Product.js";
 
 customElements.define('navigation-bar', Navigation);
 customElements.define('home-content', Home);
